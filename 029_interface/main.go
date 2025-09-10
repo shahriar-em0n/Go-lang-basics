03beb64cdbd83211cdcc57eb992a1f9e9d569b5f
shahriar-em0n <contactshahriar12@gmail.com>
2025-09-11 01:34:36 +0600

    Add files via upload

---
+++ 029_interface/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type payment struct {
+	gatway stripe
+}
+
+func (p payment) makePayment(amount  float64){
+	// razorPaymentGateWaye := razorpay{}
+
+	// stripePaymentGateway := stripe{}
+	// stripePaymentGateway.pay(amount)
+	// razorPaymentGateWaye.pay(amount)
+
+	p.gatway.pay()
+}
+
+type razorpay struct{}
+
+func (r razorpay) pay(amount float64){
+	fmt.Println("making payment using razorpay", amount)
+}
+
+type stripe struct{}
+
+func (s stripe) pay(amount float64){
+	fmt.Println("Making payment using stripe", amount)
+}
+
+func main(){
+
+	stripePaymentGateway := stripe{}
+	newPayment := payment{
+		getway: stripePaymentGateway,
+	}
+	newPayment.makePayment(100)
+}
