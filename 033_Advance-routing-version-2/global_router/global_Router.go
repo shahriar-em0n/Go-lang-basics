2d12bbc17a1c2be5f3c52681c3e7c2272520789a
shahriar-em0n <contactshahriar12@gmail.com>
2025-09-15 20:46:31 +0600

    Add global_router folder and global_router.go file

---
+++ 033_Advance-routing-version-2/global_router/global_Router.go
@@ -0,0 +1,21 @@
+package global_router
+
+import "net/http"
+
+func GlobalRouter(mux *http.ServeMux) http.Handler {
+	handleAllReq := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Content-Type", "application/json")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(200)
+			return 
+
+		} 
+
+		mux.ServeHTTP(w, r)
+	}
+
+	return http.HandlerFunc(handleAllReq)
+}
