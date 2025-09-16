3bab56b96ccd29ba689c57f120b2b3144cc8f784
shahriar-em0n <contactshahriar12@gmail.com>
2025-09-16 15:29:38 +0600

    add get_products_by_id.go

---
+++ 033_Advance-routing-version-2/handlers/get_products_by_id.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"eccomarce/database"
+	"eccomarce/util"
+	"net/http"
+	"strconv"
+)
+
+func GetProductById(w http.ResponseWriter, r *http.Request) {
+	productId := r.PathValue("productId")
+
+	pId, err := strconv.Atoi(productId)
+
+	if err != nil {
+		http.Error(w, "please give me valid product id", 400)
+		return
+	}
+
+	for _, product := range database.Carlists {
+		
+		if product.Id == pId {
+			util.SendData(w, product, 200)
+			return
+		}
+	}
+
+	util.SendData(w, "data pai nai", 404)
+
+}
